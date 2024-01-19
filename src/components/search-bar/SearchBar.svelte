8cc647e5c58cd26a9da7dbd144c8978d715ff437
Carla <Carla.candiotti@hotmail.fr>
2024-01-19 23:37:00 +0100

    create search bar

---
+++ src/components/search-bar/SearchBar.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	let searchInput: HTMLInputElement;
+
+	function checkPressedKey(event: KeyboardEvent): void {
+		if (event.key == 'Enter') {
+			search();
+		}
+	}
+
+	function search(): void {
+		console.log(searchInput.value);
+	}
+</script>
+
+<div class="input-group input-group-divider grid-cols-[auto_1fr_auto] w-[25vw]">
+	<button class="input-group-shim" on:click={search}>
+		<i class="fa-solid fa-magnifying-glass" />
+	</button>
+	<input
+		type="search"
+		placeholder="This search bar is not working for now :)"
+		on:keydown={checkPressedKey}
+		bind:this={searchInput}
+	/>
+</div>
