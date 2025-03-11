b5e872a05921409ce7f0f731f0978ba69c60f4e1
candiotti-ca <carla.candiotti@hotmail.fr>
2025-03-11 15:52:11 +0100

    feat: pplug search bar to backend request

--- src/components/search-bar/SearchBar.svelte
+++ src/components/search-bar/SearchBar.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
-	let searchInput: HTMLInputElement;
+	import { goto } from '$app/navigation';
+	import { searchProfiles } from '../../store/store';
+
+	let searchInput = $state('');
 
 	function checkPressedKey(event: KeyboardEvent): void {
 		if (event.key == 'Enter') {
@@ -8,32 +11,33 @@
 	}
 
 	function search(): void {
-		console.log(searchInput.value);
-		//TODO goto('/explore')
+		searchProfiles.update((params) => ({ ...params, firstname: searchInput }));
+		goto('/explore');
 	}
 
 	function clear(): void {
-		console.log('clear');
-	}	
+		searchInput = '';
+		searchProfiles.update((params) => ({ ...params, firstname: undefined }));
+	}
 </script>
 
 <div class="flex">
 	<div class="input-group input-group-divider grid-cols-[auto_1fr_auto] w-[25vw]">
-		<button class="input-group-shim" on:click={search}>
+		<button class="input-group-shim text-surface-900" onclick={search} aria-label="search">
 			<i class="fa-solid fa-magnifying-glass" />
 		</button>
-		
+
 		<input
-		type="search"
-		placeholder="Chercher bonheur"
-		on:keydown={checkPressedKey}
-		bind:this={searchInput}
+			type="text"
+			placeholder="Seek happiness"
+			onkeydown={checkPressedKey}
+			bind:value={searchInput}
 		/>
-		
-		{#if searchInput?.value}
-		<button class="input-group-shim" on:click={clear}>
-			<i class="fa fa-times" />
-		</button>
+
+		{#if searchInput}
+			<button onclick={clear} class="text-surface-900" aria-label="clear">
+				<i class="fa fa-times" />
+			</button>
 		{/if}
 	</div>
 </div>
